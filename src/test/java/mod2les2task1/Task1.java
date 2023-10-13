d06f09c5dfffbefbac0ef125ca882936d2edf92b
Ayrat <joevegojv@gmail.com>
2023-10-13 12:00:42 +0300

    Завершена задача 2_16

--- src/test/java/mod2les2task1/Task1.java
+++ src/test/java/mod2les2task1/Task1.java
@@ -1,10 +1,23 @@
 package mod2les2task1;
 
+import java.util.Random;
+
 public class Task1 {
 
     public static void main(String[] args) {
-        Week someWeek = new Week(5, 10);
 
-        System.out.println(someWeek);
+        Week someWeek;
+        Random random = new Random();
+        random.nextInt(4);
+        int week;
+        int day;
+
+        for (int i = 0; i < 3; i++) {
+            week = random.nextInt(52);
+            day = random.nextInt(31);
+
+            someWeek = new Week(week, day);
+            System.out.println(someWeek);
+        }
     }
 }
