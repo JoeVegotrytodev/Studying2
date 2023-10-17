664f9751392b07f03cdb29b78b78e95898072c34
Ayrat <joevegojv@gmail.com>
2023-10-17 15:51:22 +0300

    Начато написание автотеста по проверки подсчета дней

---
+++ src/test/java/mod2les25/YearsCounterTest.java
@@ -0,0 +1,16 @@
+package mod2les25;
+
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class YearsCounterTest {
+
+    @Test
+    @DisplayName("проверка корректного подсчета дней в методе getYearsByBDay")
+    @Owner("Author")
+    public void yearsCounterTest(){
+
+    }
+
+}
