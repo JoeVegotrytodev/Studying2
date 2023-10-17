f551e42ae2220983e3c5245e437636bb7481aaa0
Ayrat <joevegojv@gmail.com>
2023-10-17 15:49:29 +0300

    Дополнена задача 2_25, подсчет разницы лат вынесен в отдельный метод для тестиования

--- src/main/java/mod2les25/EntryPoint.java
+++ src/main/java/mod2les25/EntryPoint.java
@@ -8,42 +8,60 @@ import java.util.concurrent.TimeUnit;
 public class EntryPoint {
     public static void main(String[] args) {
 //        дата рождения
-//        Date birthDay = new Date(1998, Calendar.AUGUST, 4);
 
+        //для отсчета времени выполнения программы
         GregorianCalendar start = new GregorianCalendar();
 
+        //различными классами задаем дату рождения
         LocalDate bDLocalDate = LocalDate.of(1998, 8,4);
         LocalDateTime bDLocalDateTime = LocalDateTime.of(1998, 8,4, 0, 0);
         ZonedDateTime bDZonedDateTime = ZonedDateTime.of(bDLocalDate, LocalTime.of(0, 0), ZoneId.of("GMT+4"));
         GregorianCalendar bDGregorianCalendar = new GregorianCalendar(1998, Calendar.AUGUST,4);
 
-        LocalDate nowLocalDate = LocalDate.now();
-        LocalDateTime nowLocalDateTime = LocalDateTime.now();
-        ZonedDateTime nowZonedDateTime = ZonedDateTime.now();
-        GregorianCalendar nowGregorianCalendar = new GregorianCalendar();
+        //вычисление разницы дат
+        System.out.println("Years " + getYearsByBDay(bDLocalDate));
+        System.out.println("Years " + getYearsByBDay(bDLocalDateTime));
+        System.out.println("Years " + getYearsByBDay(bDZonedDateTime));
+        System.out.println("Years " + getYearsByBDay(bDGregorianCalendar));
 
-        int locDateDiff = nowLocalDate.compareTo(bDLocalDate);
-        System.out.println(locDateDiff);
-        int locDateTimeDiff = nowLocalDateTime.compareTo(bDLocalDateTime);
-        System.out.println(locDateTimeDiff);
-        int zonedDiff = nowZonedDateTime.compareTo(bDZonedDateTime);
-        System.out.println(zonedDiff);
-
-        long differenceInMillis = nowGregorianCalendar.getTimeInMillis() - bDGregorianCalendar.getTimeInMillis();
-        TimeUnit timeUnit = TimeUnit.DAYS;
-        long result = timeUnit.convert(differenceInMillis, TimeUnit.MILLISECONDS);
-        System.out.println("Сравнение дат класса GregorianCalendar: " + nowGregorianCalendar.compareTo(bDGregorianCalendar) + "\n" +
-                "Разница между датами " + result + " дня.");
-
-        for (int i = 0; i < 53323232; i++){
-            i++;
-            for (int j = 0; j < 22434545; j++){
-                j+=i;
-            }
-        }
-
+//      счетчик времени выполнения
         GregorianCalendar timerEnd = new GregorianCalendar();
         long timerDifference = timerEnd.getTimeInMillis() - start.getTimeInMillis();
-        System.out.println("Duration in mls = " + timerDifference);
+        System.out.println("\nDuration in mls = " + timerDifference);
+
+
+    }
+
+
+    static int getYearsByBDay(Object object){
+        int resultDiff;
+
+        if (object instanceof LocalDate){
+            LocalDate nowLocalDate = LocalDate.now();
+
+            resultDiff = nowLocalDate.compareTo((LocalDate)object);
+            return resultDiff;
+        }
+        else if(object instanceof LocalDateTime){
+            LocalDateTime nowLocalDateTime = LocalDateTime.now();
+
+            resultDiff = nowLocalDateTime.compareTo((LocalDateTime)object);
+            return resultDiff;
+        }else if (object instanceof ZonedDateTime){
+            ZonedDateTime nowZonedDateTime = ZonedDateTime.now();
+
+            resultDiff = nowZonedDateTime.toLocalDate().compareTo(((ZonedDateTime) object).toLocalDate());
+            return resultDiff;
+        } else if (object instanceof GregorianCalendar){
+            GregorianCalendar nowGregorianCalendar = new GregorianCalendar();
+            GregorianCalendar bDayGregorianCalendar = (GregorianCalendar)object;
+
+            long differenceInMillis = nowGregorianCalendar.getTimeInMillis() - bDayGregorianCalendar.getTimeInMillis();
+
+            TimeUnit timeUnit = TimeUnit.DAYS;
+            long days = timeUnit.convert(differenceInMillis, TimeUnit.MILLISECONDS);
+            return (int) (days / 365);
+        } else
+            throw new ClassCastException("Wrong class");
     }
 }
